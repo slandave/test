0a9e9178f4c5e5ea5ced632f9444fc23e456790d
slandave <77905470+slandave@users.noreply.github.com>
2021-02-09 14:47:23 -0600

    Add files via upload
    ZyLab 1.20

---
+++ Homework_1/ZyLab_1_20.py
@@ -0,0 +1,15 @@
+# Santiago Landaverde
+# 1856681
+user_num = int(input('Enter integer:\n'))
+print('You entered:', user_num)
+user_num1 = user_num*user_num
+print(user_num, "squared is", user_num1)
+user_num2 = user_num*user_num*user_num
+print('And', user_num, 'cubed is', user_num2, '!!')
+user_num3 = int(input('Enter another integer:\n'))
+user_num4 = user_num + user_num3
+print(user_num, '+', user_num3, "is", user_num4)
+user_num5 = user_num * user_num3
+print(user_num, '*', user_num3, "is", user_num5)
+
+
